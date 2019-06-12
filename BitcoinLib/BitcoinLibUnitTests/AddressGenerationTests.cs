9ed58162cd77e283bbbde6b4f99aea3721204594
Tim <TimCodesStuff@hotmail.com>
2019-06-12 00:56:16 -0700

    Adding Unit Tests.
    Adding a few unit tests for bitcoin address generation. Somewhere out there DriveHappy is smiling, knowing that this code is now tested.

---
+++ BitcoinLib/BitcoinLibUnitTests/AddressGenerationTests.cs
@@ -0,0 +1,81 @@
+﻿using BitcoinLib;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BitcoinLibUnitTests
+{
+    /// <summary>
+    /// Tests that Bitcoin Address generation works correctly and fails when expected. 
+    /// WARNING: Do not use the addresses tested here to store bitcoin!
+    /// See https://en.bitcoin.it/wiki/Technical_background_of_version_1_Bitcoin_addresses for more info on this address.
+    /// </summary>
+    [TestClass]
+    public class AddressGenerationTests
+    {
+        // Binary Key Tests
+        [TestMethod]
+        public void GenerateAddressFromKnownGoodPrivateKey()
+        {
+            Byte[] key = Encoding.HexStringToByteArray("18e14a7b6a307f426a94f8114701e7c8e774e7f9a47e2c2035db29a206321725");
+            (BitcoinAddress address, String result) = BitcoinAddress.CreateAddressFromPrivateKeyByteArray(key, NetworkType.Main);
+            Assert.IsNotNull(address);
+            Assert.AreEqual(address.PrivateKey, "18e14a7b6a307f426a94f8114701e7c8e774e7f9a47e2c2035db29a206321725");
+            Assert.AreEqual(address.PrivateKeyWIF, "Kx45GeUBSMPReYQwgXiKhG9FzNXrnCeutJp4yjTd5kKxCitadm3C");
+            Assert.AreEqual(address.P2PKHAddress, "1PMycacnJaSqwwJqjawXBErnLsZ7RkXUAs");
+            Assert.AreEqual(address.P2SHAddress, "3Q3zY87DrUmE371Grgc7bsDiVPqpu4mN1f");
+        }
+
+        [TestMethod]
+        public void InvalidBinaryKeyLengthTest()
+        {
+            int byteArrayLength = 33;
+            Byte[] key = new Byte[byteArrayLength];
+            (BitcoinAddress address, String result) = BitcoinAddress.CreateAddressFromPrivateKeyByteArray(key, NetworkType.Main);
+            Assert.IsNull(address);
+            Assert.AreEqual(result, $"Private key has invalid length of {byteArrayLength}, expected length of 32.");
+        }
+
+
+        // Hexadecimal Tests
+        [TestMethod]
+        public void GenerateAddressFromKnownGoodPrivateKeyHex()
+        {
+            (BitcoinAddress address, String result) = BitcoinAddress.CreateAddressFromPrivateKeyHex("18e14a7b6a307f426a94f8114701e7c8e774e7f9a47e2c2035db29a206321725", NetworkType.Main);
+            Assert.IsNotNull(address);
+            Assert.AreEqual(address.PrivateKeyWIF, "Kx45GeUBSMPReYQwgXiKhG9FzNXrnCeutJp4yjTd5kKxCitadm3C");
+            Assert.AreEqual(address.P2PKHAddress, "1PMycacnJaSqwwJqjawXBErnLsZ7RkXUAs");
+            Assert.AreEqual(address.P2SHAddress, "3Q3zY87DrUmE371Grgc7bsDiVPqpu4mN1f");
+        }
+
+        [TestMethod]
+        public void InvalidHexadecimalKeyLengthTest()
+        {
+            string privateKeyHex = "118e14a7b6a307f426a94f8114701e7c8e774e7f9a47e2c2035db29a206321725";
+            (BitcoinAddress address, String result) = BitcoinAddress.CreateAddressFromPrivateKeyHex(privateKeyHex, NetworkType.Main);
+            Assert.IsNull(address);
+            Assert.AreEqual(result, $"Private key hex string has invalid length of {privateKeyHex.Length}, expected length of 64.");
+        }
+
+        [TestMethod]
+        public void InvalidHexadecimalCharacterTest()
+        {
+            (BitcoinAddress address, String result) = BitcoinAddress.CreateAddressFromPrivateKeyHex("Z8e14a7b6a307f426a94f8114701e7c8e774e7f9a47e2c2035db29a206321725", NetworkType.Main);
+            Assert.IsNull(address);
+            Assert.AreEqual(result, "Private key is not in hexadecimal format. Unexpected character in string.");
+        }
+
+
+        // WIF tests
+        [TestMethod]
+        public void GenerateAddressFromKnownGoodPrivateKeyWIF()
+        {
+            (BitcoinAddress address, String result) = BitcoinAddress.CreateAddressFromPrivateKeyWIF("Kx45GeUBSMPReYQwgXiKhG9FzNXrnCeutJp4yjTd5kKxCitadm3C", NetworkType.Main);
+            Assert.IsNotNull(address);
+            Assert.AreEqual(address.PrivateKey, "18e14a7b6a307f426a94f8114701e7c8e774e7f9a47e2c2035db29a206321725");
+            Assert.AreEqual(address.P2PKHAddress, "1PMycacnJaSqwwJqjawXBErnLsZ7RkXUAs");
+            Assert.AreEqual(address.P2SHAddress, "3Q3zY87DrUmE371Grgc7bsDiVPqpu4mN1f");
+        }
+
+        //TODO: Add more tests once WIF validation is implemented.
+    }
+}
---
+++ BitcoinLib/BitcoinLibUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BitcoinLibUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BitcoinLibUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("66d0cdb9-e736-4e4c-90f1-19829b1905fb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
